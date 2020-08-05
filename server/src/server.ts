890c3da121400b1b877472c5fffe31ccf7b4b814
ma. fernanda rodriguez r <mafda13@gmail.com>
2020-08-05 14:55:47 -0300

    setting database connection and criate tables database

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  return response.send('hello');
-});
+app.use(express.json());
+app.use(routes);
 
 // localhost:3333
 app.listen(3333);
