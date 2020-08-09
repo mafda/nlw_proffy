dddb8fe6f4323c4aa6d978aeeb4cc68bb0d79e36
ma. fernanda rodriguez r <mafda13@gmail.com>
2020-08-08 21:11:32 -0300

    favorites list

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -3,6 +3,7 @@ import { View, ScrollView, Text, TextInput } from 'react-native';
 import { BorderlessButton, RectButton } from 'react-native-gesture-handler';
 import { Feather } from '@expo/vector-icons';
 import AsyncStorage from '@react-native-community/async-storage';
+import { useFocusEffect } from '@react-navigation/native';
 
 import PageHeader from '../../Components/PageHeader';
 import TeacherItem, { Teacher } from '../../Components/TeacherItem';
@@ -33,6 +34,12 @@ function TeacherList() {
       }
     });
   }
+
+  useFocusEffect(
+    React.useCallback(() => {
+      loadFavorites();
+    }, [])
+  )
 
   function handleToggleFiltersVisible() {
     setIsFilterVisible(!isFilterVisible);
